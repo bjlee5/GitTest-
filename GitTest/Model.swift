b8c8f08000144a532470d71a8cad327c10267864
Brian Lee <brianjacoblee@gmail.com>
2017-03-17 09:36:55 -0400

    Dyeuck has been introduced

---
+++ GitTest/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  GitTest
+//
+//  Created by MacBook Air on 3/17/17.
+//  Copyright © 2017 LionsEye. All rights reserved.
+//
+
+import Foundation
+
+struct Dyeuck {
+    var tits: String
+    var butts: String
+    var dicks: String
+    var fucks: String
+}
