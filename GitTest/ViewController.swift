77512a3151226665dc484527b4a2a9ae285d0fe7
Brian Lee <brianjacoblee@gmail.com>
2017-03-17 09:02:49 -0400

    newArray added

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     var someArray = ["one", "two", "three"]
+    var newArray = ["dicks", "butts", "tits"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
